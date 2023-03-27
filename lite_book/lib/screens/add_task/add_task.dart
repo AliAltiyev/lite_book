7daf8f1b9a6fbae513a2d3596c48cbb3b1c6e81c
Abdusselam <alidroid696@gmail.com>
2023-03-27 12:12:43 +0300

    Add test libarary

--- lite_book/lib/screens/add_task/add_task.dart
+++ lite_book/lib/screens/add_task/add_task.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:lite_book/screens/add_task/add_task_controller.dart';
 
-class AddTaskPage extends StatelessWidget {
+class AddTaskPage extends GetWidget<AddTAskController> {
   const AddTaskPage({super.key});
   final String pageName = '/AddTaskPage';
 
